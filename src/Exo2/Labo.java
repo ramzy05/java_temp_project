20a5486b7890760ecbf8c9b042ce0e0415c547a6
ramzy05 <ramsesaguegang@gmail.com>
2023-01-14 10:54:15 +0100

    partie de un exo2 fonctionnelle

--- src/Exo2/Labo.java
+++ src/Exo2/Labo.java
@@ -6,7 +6,57 @@ class Souris {
     /*******************************************
      * Completez le programme a partir d'ici.
      *******************************************/
+    private int poids;
+    private String couleur;
+    private int age = 0;
+    private int esperanceVie;
+    private boolean clonee = false;
 
+    public Souris(int pds, String col, int ag){
+        poids = pds;
+        couleur = col;
+        age = ag;
+        esperanceVie = ESPERANCE_VIE_DEFAUT;
+        System.out.println("Une nouvelle souris");
+    }
+
+    public Souris(int pds, String col){
+        poids = pds;
+        couleur = col;
+        esperanceVie = ESPERANCE_VIE_DEFAUT;
+        System.out.println("Une nouvelle souris !");
+    }
+
+    public Souris(Souris obj){
+        System.out.println("Clonage d'une souris !");
+        poids = obj.poids;
+        couleur = obj.couleur;
+        age = obj.age;
+        esperanceVie = obj.esperanceVie/4;
+        clonee = true;
+        System.out.println("Une nouvelle souris !");
+
+    }
+
+    public String toString(){
+        String tmp = (clonee) ? ", clonee,":"";
+        return "Une souris "+couleur+ tmp +" de " + age +" mois et pesant "+ poids +" grammes";
+    }
+
+    public void veillir(){
+        age += 1;
+        if(clonee){
+            if(age > esperanceVie/2){
+                couleur = "verte";
+            }
+        }
+    }
+
+    public void evolue(){
+        while (age < esperanceVie) {
+            veillir();
+        }
+    }
 }
 
 /*******************************************
